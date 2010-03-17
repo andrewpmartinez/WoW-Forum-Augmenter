bab9c8991b293d7b3549dfff317a80b4a6416e03
Raegx <andrew.p.martinez@gmail.com>
2010-03-17 02:28:37 -0400

    -Removed debug msgs

--- WFA_Chrome/wow_forum_augmentor.user.js
+++ WFA_Chrome/wow_forum_augmentor.user.js
@@ -6,7 +6,7 @@
 // @include		http://forums.wow-europe.com/*
 // ==/UserScript==
 
-var WFA_VERSION = "_____15_____";
+var WFA_VERSION = "_____16_____";
 
 
 //Name space for all WoW Forum related operations
@@ -193,8 +193,7 @@ var WFA =
 		realm = realm.replace( /'/g, '-' ).replace( /\s/g, "-").toLowerCase();
 		guild = guild.replace( /'/g, '-' ).replace( /\s/g, "+");
 		var requestUrl = 'http://www.wowprogress.com/guild/'+area+'/'+escape(realm)+'/'+escape(guild)+'/json_rank';
-		console.log( requestUrl );
-		console.log( escape( requestUrl ) );
+
         if( WFA.isChrome() )
         {
         	chrome.extension.sendRequest({'action' : 'fetchGuildRank', 'requestUrl':requestUrl}, function(responseDetails){WFA.onRequest( responseDetails, key,area, realm, guild, callBack)});
